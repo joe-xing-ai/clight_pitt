f972142a391ac5d563577f754d8488ec168159de
Joe Xing <joe.xing.ai@gmail.com>
2021-11-05 15:14:13 +0800

    added text flag

--- upload_data.py
+++ upload_data.py
@@ -8,20 +8,27 @@ import datetime
 from aws_api import aws_api
 
 
-def upload_videos_to_cloud(s3, data_path, bucket):
+def upload_videos_to_cloud(s3, data_path, bucket, text):
 
     video_entries = []
     for root, dirs, files in os.walk(data_path):
         for file in files:
-            # Note: we only look at avi or mp4 video files
-            if file.endswith(".mp4") or file.endswith(".avi") or file.endswith(".MP4") or file.endswith(".AVI"):
-                file_path = os.path.join(root, file)
-                cloud_key = file_path
-                tuple_entry = (file_path, cloud_key)
-                video_entries.append(tuple_entry)
+            if text:
+                if file.endswith(".txt") or file.endswith(".TXT") or file.endswith(".csv") or file.endswith(".CSV"):
+                    file_path = os.path.join(root, file)
+                    cloud_key = file_path
+                    tuple_entry = (file_path, cloud_key)
+                    video_entries.append(tuple_entry)
+            else:
+                # Note: we only look at avi or mp4 video files
+                if file.endswith(".mp4") or file.endswith(".avi") or file.endswith(".MP4") or file.endswith(".AVI"):
+                    file_path = os.path.join(root, file)
+                    cloud_key = file_path
+                    tuple_entry = (file_path, cloud_key)
+                    video_entries.append(tuple_entry)
 
     n_videos = len(video_entries)
-    logging.info("upload_data: found %s videos files, getting ready to upload now..." % n_videos)
+    logging.info("upload_data: found %s files, getting ready to upload now..." % n_videos)
 
     for i in range(n_videos):
         tuple_entry = video_entries[i]
@@ -29,7 +36,7 @@ def upload_videos_to_cloud(s3, data_path, bucket):
 
         video_object = s3.Object(bucket, cloud_key)
 
-        logging.info("upload video %s out of total %s videos: %s to %s ... " % (i, n_videos, file_path, cloud_key))
+        logging.info("upload file %s out of total %s files: %s to %s ... " % (i, n_videos, file_path, cloud_key))
         video_object.upload_file(file_path)
 
     logging.info("upload to aws s3 bucket is done!")
@@ -47,13 +54,15 @@ def main():
     parser.add_option('--data_path', action="store", default="./data/", help=None)
     parser.add_option('--bucket', action="store", default="clightpitt", help=None)
     parser.add_option('--credential', action='store', default='clight_pitt.csv', help=None)
+    parser.add_option('--text', action="store_true", default=False, help=None)
+
     options, args = parser.parse_args()
 
     logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s', level=logging.INFO)
 
     s3_resource = aws_api(options.credential, "us-east-2")
 
-    upload_videos_to_cloud(s3_resource, options.data_path, options.bucket)
+    upload_videos_to_cloud(s3_resource, options.data_path, options.bucket, options.text)
 
 
 if __name__ == "__main__":
